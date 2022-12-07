7cb59422a1228eac86226087a5e0caa0a10392da
Chris Choy <chrischoythehuman@gmail.com>
2022-12-07 01:14:33 -0500

    added a template for copying

---
+++ day8/code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""https://adventofcode.com/2022/day/6"""
+import sys
+__author__ = "Chris Choy"
+
+assert(len(sys.argv) == 2) # python3 dN.py input.txt
+
+with open(sys.argv[1],'r') as f:
+    for l in f.readlines():
+        pass
+    print("ANS:",)
