10ee1778ec27b92cb8ab179478f653e7c0747a87
Chris Choy <chrischoythehuman@gmail.com>
2022-12-09 00:49:11 -0500

    finished day 9

--- day9/code.py
+++ day9/code.py
@@ -8,7 +8,7 @@ __author__ = "Chris Choy"
 assert(len(sys.argv) == 2) # python3 dN.py input.txt
 
 visited = set()
-def tailmoves(hpos, tpos):
+def follows(hpos, tpos):
     hx,hy = hpos 
     tx,ty = tpos
     xdist = abs(hx-tx)
@@ -28,25 +28,26 @@ def move(pos, dir):
     return (x,y)
 
 with open(sys.argv[1],'r') as f:
-    hpos = (0,0)
-    tpos = (0,0)
-    visited.add(tpos)
+    rope = [(0,0)] * 10 # H,1,2,...,9
+    visited.add(rope[-1])
     for l in f.readlines():
         dir, steps = l.strip().split(' ')
-        # print("===={} {}====".format(dir,steps))
+        print("===={} {}====".format(dir,steps))
         for _ in range(int(steps)):
-            hpos = move(hpos,dir)
-            if tailmoves(hpos,tpos):
-                hx,hy = hpos
-                tx,ty = tpos 
-                if tx < hx:
-                    tpos = move(tpos,'R')
-                if tx > hx:
-                    tpos = move(tpos,'L')
-                if ty < hy:
-                    tpos = move(tpos,'U')
-                if ty > hy:
-                    tpos = move(tpos,'D')
-            visited.add(tpos)
-            # print(hpos,tpos)
+            rope[0] = move(rope[0],dir)
+            for i in range(1,len(rope)):
+                if follows(rope[i-1],rope[i]):
+                    x1,y1 = rope[i-1]
+                    x2,y2 = rope[i]
+                    if x2 < x1:
+                        x2 += 1
+                    if x2 > x1:
+                        x2 -= 1
+                    if y2 < y1:
+                        y2 += 1
+                    if y2 > y1:
+                        y2 -= 1
+                    rope[i] = (x2,y2)
+            visited.add(rope[-1])
+            print(rope)
     print("ANS:", len(visited))
