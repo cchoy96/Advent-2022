6d9f9ee4f4a70facb9910481b4b2eca2b818292a
Chris Choy <chrischoythehuman@gmail.com>
2022-12-10 01:05:40 -0500

    solved day 10. cole beat me so im sad

--- day10/code.py
+++ day10/code.py
@@ -15,14 +15,41 @@ def myprint(s):
     if DEBUG: 
         print(s)
 
-def solution(f):
-    ans = []
-    with open(f,'r') as f:
-        for l in f.readlines():
-            pass
-    return ans
+class Solution():
+    def __init__(self, f):
+        self.f = f
+        self.cycle = 0
+        self.x = 1
+        self.vals = []
+        self.grid = ""
 
+    def run_cycle(self):
+        if self.cycle in [40,80,120,160,200,240]:
+            self.grid += '\n'
+        c = '#' if self.cycle%40 in range(self.x-1, self.x+2) else '.'
+        self.grid += c
+
+        self.cycle += 1
+        if self.cycle in [20,60,100,140,180,220]:
+            self.vals.append(self.x * self.cycle)
+
+    def answer(self):
+        with open(self.f,'r') as f:
+            for l in f.readlines():
+                toks = l.strip().split(' ')
+                if toks[0] == "noop":
+                    self.run_cycle()
+                elif toks[0] == "addx":
+                    val = int(toks[1])
+                    for _ in range(2):
+                        self.run_cycle()
+                    self.x += val
+                # if self.cycle > 40:
+                #     break
+        print(self.grid)
+        return sum(self.vals)
 
 #####
-assert(solution(example) == _)
-# print(solution(problem))
+
+print(Solution(example).answer() == 13140)
+print(Solution(problem).answer() == 10760)
